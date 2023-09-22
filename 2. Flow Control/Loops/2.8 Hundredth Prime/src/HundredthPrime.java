286a4e7dd0dbc547fe9dd3a9ce653dd4f017f38b
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-09-22 15:05:01 +0200

    last update, all activities finished!

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -1,21 +1,17 @@
 public class HundredthPrime {
     public static void main(String[] args) {
-        int count = 1; //count stores the amount of primes found
-        int num = 1; //num is the current number being evaluated
-        while (count < 100) //while the 100th prime hasn't been found
-        {
-            num++; //at the start of every attempt, increase the value of num by 1 (so we start looking for primes at 2)
-            for (int div = num - 1; div > 1; div--) //div is the number currently acting as the divisor, which starts as one less than the current number
-            {
-                if (num % div == 0) //if the divisor divides the number exactly
-                {
-                    break; //then this is not a prime, keep looking
-                } else if (div == 2) //if all the divisors down to 2 have been evaluated without success, then this is a prime
-                {
-                    count++; //increase the prime count by one
+        int count = 1;
+        int num_current = 1;
+        while (count < 100) {
+            num_current++;
+            for (int div_current = num_current - 1; div_current > 1; div_current--) {
+                if (num_current % div_current == 0) {
+                    break;
+                } else if (div_current == 2) {
+                    count++;
                 }
             }
         }
-        System.out.println(num); //print the 100th prime
+        System.out.println(num_current);
     }
 }
